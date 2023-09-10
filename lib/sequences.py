a37f8949dd04913c99a9fd939c937429bff89ddf
LuisXalcedo <luis.e.ss@gmail.com>
2023-09-10 12:46:21 -0600

    complete sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    cache = {0: 0, 1: 1}
+
+    def fibonacci_of(n):
+        if n in cache:  # Base case
+            return cache[n]
+        cache[n] = fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
+        return cache[n]
+    print([fibonacci_of(n) for n in range(length)])
